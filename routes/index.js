e170435cfb239a22f93ea07bd9c70a479f4d48da
snvn1511 <spxedu@gmail.com>
2023-03-24 10:36:35 +0000

    Update 6 files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
+const { render } = require("ejs");
+var express = require("express");
 var router = express.Router();
+var multer = require("multer");
+var up = multer({ dest: "./tmp" }).single("abc");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Hêllo 22222" });
+});
+
+router.post("/", up, (req, res, next) => {
+  console.log(req.file);
 });
 
 module.exports = router;
